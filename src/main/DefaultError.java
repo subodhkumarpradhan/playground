31fb8c5d105eea6bee8eb074a2218ba69734a366
Subodh Kumar Pradhan <subodh.msu@gmail.com>
2023-08-23 00:30:51 +0530

    US-02 - DefaultError class fields are added

--- src/main/DefaultError.java
+++ src/main/DefaultError.java
@@ -1,4 +1,43 @@
 package main;
 
+import java.util.Date;
+
 public class DefaultError {
+
+    private Date date;
+    private Long code;
+    private String text;
+
+    public DefaultError() {
+    }
+
+    public DefaultError(Date date, Long code, String text) {
+        this.date = date;
+        this.code = code;
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Long getCode() {
+        return code;
+    }
+
+    public void setCode(Long code) {
+        this.code = code;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
